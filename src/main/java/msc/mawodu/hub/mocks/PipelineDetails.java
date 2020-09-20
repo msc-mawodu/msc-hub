7bbda82317cc2882db527bd02a8b251d275306b2
MaWoDu <durkiewiczmaciej@gmail.com>
2020-09-20 21:35:42 +0100

    Added basic support for the pipeline details page.

---
+++ src/main/java/msc/mawodu/hub/mocks/PipelineDetails.java
@@ -0,0 +1,29 @@
+package msc.mawodu.hub.mocks;
+
+import msc.mawodu.hub.PipelineOverview;
+
+import java.util.List;
+
+public class PipelineDetails {
+    private PipelineOverview details;
+    private String notes;
+    private List<String> filePaths;
+
+    public PipelineDetails(PipelineOverview details, String notes, List<String> filePaths) {
+        this.details = details;
+        this.notes = notes;
+        this.filePaths = filePaths;
+    }
+
+    public PipelineOverview getDetails() {
+        return details;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public List<String> getFilePaths() {
+        return filePaths;
+    }
+}
