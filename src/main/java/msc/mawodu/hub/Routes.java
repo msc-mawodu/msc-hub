df437ea33ab2c819752c26a12f20537c64dbaa1f
MaWoDu <durkiewiczmaciej@gmail.com>
2020-09-26 22:24:03 +0100

    Minor fixes to upload controller.

--- src/main/java/msc/mawodu/hub/Routes.java
+++ src/main/java/msc/mawodu/hub/Routes.java
@@ -5,7 +5,7 @@ public class Routes {
     public static final String HOME = "/";
     public static final String PIPELINE_DETAILS = "/pipeline/{uid}";
     public static final String PIPELINE_NOTES_UPDATE = "/note/{pipelineId}";
-    public static final String PIPELINE_FILE_UPLOAD = "/file/{pipelineId}";
+    public static final String PIPELINE_FILE_UPLOAD = "/upload/{pipelineId}";
     public static final String PIPELINE_MANUAL_FILE_UPLOAD = "/file/manual/{pipelineId}";
     public static final String PIPELINE_FILE_DOWNLOAD = "/file/{pipelineId}/{fileName}";
     public static final String PIPELINE_STATUS_UPLOAD = "/status/{pipelineId}";
--- src/main/java/msc/mawodu/hub/fileUpload/FileUploadController.java
+++ src/main/java/msc/mawodu/hub/fileUpload/FileUploadController.java
@@ -6,10 +6,12 @@ import msc.mawodu.hub.fileDownload.FilenamesStore;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -28,16 +30,14 @@ public class FileUploadController {
     }
 
     @PostMapping(value = Routes.PIPELINE_FILE_UPLOAD)
-    public String handleFileUpload(@RequestParam("file") MultipartFile file, @PathVariable String pipelineId) {
+    @ResponseStatus(HttpStatus.OK)
+    public void handleFileUpload(@RequestParam("file") MultipartFile file, @PathVariable String pipelineId) {
         logger.info(String.format("Received file to be uploaded: %s", file.getOriginalFilename()));
 
         boolean fileStoredSuccessfully = fileResolver.storeFile(file, pipelineId);
         if(fileStoredSuccessfully) {
             filenamesStore.registerNewFile(pipelineId, file.getOriginalFilename());
-            return "OK";
         }
-
-        return "ERROR";
     }
 
     @PostMapping(value = Routes.PIPELINE_MANUAL_FILE_UPLOAD)
