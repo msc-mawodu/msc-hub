3e3045630ae9b8bee6c35c80c931f67f10393275
MaWoDu <durkiewiczmaciej@gmail.com>
2020-09-26 21:59:02 +0100

    Improved frontend support to for file upload.

--- src/main/java/msc/mawodu/hub/mocks/MockInMemoryFileNamesDatabase.java
+++ src/main/java/msc/mawodu/hub/mocks/MockInMemoryFileNamesDatabase.java
@@ -1,21 +1,22 @@
 package msc.mawodu.hub.mocks;
 
 import msc.mawodu.hub.fileDownload.FilenamesStore;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class MockInMemoryFileNamesDatabase implements FilenamesStore {
+
+    private static final Logger logger = LoggerFactory.getLogger(MockInMemoryFileNamesDatabase.class);
 
     private static Map<String, List<String>> mockDB;
 
     static {
         mockDB = new HashMap<>();
-        mockDB.put("RNASeQC", Arrays.asList("qc.txt", "report.txt", "performance.txt"));
-        mockDB.put("SolexaQA", Arrays.asList("qc.txt", "report.txt", "performance.txt"));
-        mockDB.put("FastqMcf", Arrays.asList("qc.txt", "report.txt", "performance.txt"));
+        mockDB.put("RNASeQC", new ArrayList<String>(Arrays.asList("qc.txt", "report.txt", "performance.txt")));
+        mockDB.put("SolexaQA", new ArrayList<String>(Arrays.asList("qc.txt", "report.txt", "performance.txt")));
+        mockDB.put("FastqMcf", new ArrayList<String>(Arrays.asList("qc.txt", "report.txt", "performance.txt")));
     }
 
     @Override
@@ -27,8 +28,10 @@ public class MockInMemoryFileNamesDatabase implements FilenamesStore {
     public boolean registerNewFile(String pipelineId, String fileName) {
         try {
             mockDB.get(pipelineId).add(fileName);
+            logger.info("Stored new filename in db");
             return true;
         } catch (Exception e) {
+            logger.error(String.format("Unable to stored new filename in db for: %s %s", pipelineId, fileName));
             return false;
         }
     }
