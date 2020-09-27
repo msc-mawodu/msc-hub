c7681faf1d4fadf7f22cc0b76475adf809c1abd4
MaWoDu <durkiewiczmaciej@gmail.com>
2020-09-27 15:36:53 +0100

    Updated Test notes.

--- src/main/java/msc/mawodu/hub/development/MockFileResolver.java
+++ src/main/java/msc/mawodu/hub/development/MockFileResolver.java
@@ -1,5 +1,6 @@
-package msc.mawodu.hub.files;
+package msc.mawodu.hub.development;
 
+import msc.mawodu.hub.files.FileResolver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
